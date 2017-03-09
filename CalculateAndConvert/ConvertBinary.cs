c3fabd84376dda30b39bdfbdc0aa2c255404d2ee
alexpain <alex.pain.6@gmail.com>
2017-03-09 13:04:07 +0300

    Create ClassLibrary

--- CalculateAndConvert/ConvertBinary.cs
+++ CalculateAndConvert/ConvertBinary.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace MVC_T1
+namespace CalculateAndConvert
 {
     public class ConvertBinary
     {
--- MVC_T1/Controllers/HomeController.cs
+++ MVC_T1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CalculateAndConvert;
 
 namespace MVC_T1.Controllers
 {
